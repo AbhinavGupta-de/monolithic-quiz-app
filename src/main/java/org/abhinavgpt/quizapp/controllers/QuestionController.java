3a87e1faab0236c82ebd5a18850760b422c8ec12
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-05-13 00:12:38 +0530

    fix: fixed controller, dto and onfiguration in application properties

--- src/main/java/org/abhinavgpt/quizapp/controllers/QuestionController.java
+++ src/main/java/org/abhinavgpt/quizapp/controllers/QuestionController.java
@@ -1,34 +1,49 @@
 package org.abhinavgpt.quizapp.controllers;
 
+import org.abhinavgpt.quizapp.dtos.AddQuestionDTO;
+import org.abhinavgpt.quizapp.model.Question;
+import org.abhinavgpt.quizapp.servies.questionservice.IQuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/questions")
 public class QuestionController {
 
+    IQuestionService questionService;
+
+    @Autowired
+    public QuestionController(IQuestionService questionService) {
+        this.questionService = questionService;
+    }
+
     @GetMapping("")
-    public String getQuestions() {
-        return "Questions";
+    public List<Question> getQuestions() {
+        return questionService.getQuestions();
     }
 
     @GetMapping("/{id}")
-    public String getQuestionById() {
-        return "Question by id";
+    public Question getQuestionById(@PathVariable Long id) {
+        return questionService.getQuestionById(id);
     }
 
     @PostMapping("")
-    public String addQuestion() {
-        return "Add question";
+    public Question addQuestion(@RequestBody AddQuestionDTO addQuestionDTO) {
+        return questionService.addQuestion(addQuestionDTO);
     }
 
-    @PostMapping("/{id}")
-    public String updateQuestion() {
-        return "Update question";
+    @PutMapping("")
+    public Question updateQuestion(@RequestBody Question question) {
+        return questionService.updateQuestion(question);
+
     }
 
     @DeleteMapping("/{id}")
-    public String deleteQuestion() {
-        return "Delete question";
+    public String deleteQuestion(@PathVariable Long id) {
+        questionService.deleteQuestion(id);
+        return "Question deleted successfully";
     }
 
 }
--- src/main/java/org/abhinavgpt/quizapp/dtos/AddQuizDTO.java
+++ src/main/java/org/abhinavgpt/quizapp/dtos/AddQuizDTO.java
@@ -2,5 +2,5 @@ package org.abhinavgpt.quizapp.dtos;
 
 import java.util.List;
 
-public record AddQuizDTO(String name, String description, Long categoryId, Long userId, List<Long> questionIds) {
+public record AddQuizDTO(String name, String description, List<Long> questionIds) {
 }
